dd27f35f9307d8c0a5a46f4aa8ab5c2f4c9f61ce
Thong LT <letrthong@gmail.com>
2022-09-15 21:00:30 +0700

    ssl

---
+++ src/gunicorn_local.sh
@@ -0,0 +1,10 @@
+ 
+# pip3 install gunicorn
+# sudo apt install nginx
+ 
+export PYTHONPATH="${PYTHONPATH}:/app"
+
+gunicorn   --certfile=server.crt --keyfile=server.key    --bind 0.0.0.0:8443 --workers 2 --thread 8    wsgi:app 
+
+
+# python3 wsgi.py
---
+++ src/wsgi.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+# 
+if __name__ == "__main__":
+    # app.run(debug=True, host="0.0.0.0", port=443,  ssl_context=('/app/src/server.crt', '/app/src/server.key') )
+    #app.run(debug=True, host="0.0.0.0", port=8080   )
+    app.run(debug=True, host="0.0.0.0", port=8443,  ssl_context=('./server.crt', './server.key') )
