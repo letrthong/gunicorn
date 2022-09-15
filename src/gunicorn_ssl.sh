91c8e29d18b9608b1bdae9d1a2f3abd17020ebfe
Thong LT <letrthong@gmail.com>
2022-09-15 21:51:08 +0700

    clean up

---
+++ src/gunicorn_ssl.sh
@@ -0,0 +1,10 @@
+ 
+# pip3 install gunicorn
+# sudo apt install nginx
+ 
+export PYTHONPATH="${PYTHONPATH}:/app"
+
+gunicorn   --certfile=server.crt --keyfile=server.key    --bind 10.240.0.31:443 --workers 2 --thread 8    wsgi_ssl:app 
+
+
+# python3 wsgi.py
