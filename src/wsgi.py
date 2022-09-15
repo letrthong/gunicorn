f8263a1f6bf96a351e5598c6a5d48c044397a574
Thong LT <letrthong@gmail.com>
2022-09-15 21:35:27 +0700

    Update wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -10,5 +10,10 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
+
+@app.route("/health_check")
+def health_check():
+    return "{}"
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=443,  ssl_context=('./server.crt', './server.key') )
