87cafb8a5552115568c245f7b4951bea18ffd755
Thong LT <letrthong@gmail.com>
2022-09-15 21:08:51 +0700

    Create requirements.sh

---
+++ src/requirements.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install python
+
+
+pip  install gunicorn
+pip  install Flask
+pip install pyopenssl
+
+pip3 install gunicorn
+pip3 install Flask
+pip3 install pyopenssl
+ 
