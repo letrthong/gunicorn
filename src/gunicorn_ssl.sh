395c138a136e151486bbc73a8852a0c9a677570c
Thong LT <letrthong@gmail.com>
2022-09-16 09:33:00 +0700

    Update gunicorn_ssl.sh

--- src/gunicorn_ssl.sh
+++ src/gunicorn_ssl.sh
@@ -4,7 +4,7 @@
  
 export PYTHONPATH="${PYTHONPATH}:/app"
 
-gunicorn   --certfile=server.crt --keyfile=server.key    --bind 0.0.0.0:443 --workers 2 --thread 8    wsgi_ssl:app  --daemon
+gunicorn   --certfile=server.crt --keyfile=server.key  --bind 0.0.0.0:443 --workers 1 --thread 8 wsgi_ssl:app  --daemon
 
 
 # python3 wsgi.py
