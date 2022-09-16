26bcec8212f4a1d8ab93db76b24f40615c8bffd8
Thong LT <letrthong@gmail.com>
2022-09-16 05:09:40 +0200

    Test

--- src/gunicorn_ssl.sh
+++ src/gunicorn_ssl.sh
@@ -4,7 +4,8 @@
  
 export PYTHONPATH="${PYTHONPATH}:/app"
 
-gunicorn   --certfile=server.crt --keyfile=server.key  --bind 0.0.0.0:443 --workers 1 --thread 8 wsgi_ssl:app  --daemon
+#gunicorn   --certfile=server.crt --keyfile=server.key  --bind 0.0.0.0:443 --workers 1 --thread 8 wsgi_ssl:app  --daemon
+gunicorn   --certfile=server.crt --keyfile=server.key  --bind 0.0.0.0:443 --workers 1 --thread 8 wsgi_ssl:app  
 
 
 # python3 wsgi.py
